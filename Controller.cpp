f8dd82960d4867bb0a0d2af39fde00ad0a250133
Callum Metcalfe <callummgamedev@hotmail.com>
2020-11-26 16:38:18 +0000

    - Task 4 Completed whooo

--- Controller.cpp
+++ Controller.cpp
@@ -26,7 +26,6 @@ float Controller::calculateShotSpeed(const Vector2& tankPos, const Vector2& enem
 	 * Then sqrt the result to get u =
 	 * u = sqrt(1/2 * g * Sx / cos(x) * sin(x))
 	 */
-	// Part 1 formula
 	//const float initialVelocity = sqrt((displacement.x * (0.5 * gravity)) / (cos(shotAngleRadians) * sin(shotAngleRadians)));
 
 	/**
@@ -43,7 +42,6 @@ float Controller::calculateShotSpeed(const Vector2& tankPos, const Vector2& enem
 	 * Do the same again but divide by (ucos(x))^2 and sqrt the end result to get u = 
 	 * u = sqrt(-g * Sx^2 / 2 * cos(x)^2 * Sy - tan(x)
 	 */
-	// Part 2 formula 
 	//const float initialVelocity = sqrt((-gravity * pow(displacement.x, 2)) / (2 * pow(cos(shotAngleRadians), 2) * (-displacement.y - tan(shotAngleRadians) * displacement.x)));
 
 	/**
@@ -58,12 +56,11 @@ float Controller::calculateShotSpeed(const Vector2& tankPos, const Vector2& enem
 	 * rearrange to get u^2 = gravity * a^2 / 2(a * sin(x) - Sy)
 	 * then square root the formula to get u
 	 */
-
 	const float alpha = (gravity * displacement.x + wind * -displacement.y) / (gravity * cos(shotAngleRadians) + wind * sin(shotAngleRadians));
 	
 	const float initialVelocity = sqrt((gravity * pow(alpha, 2)) / (2 * (alpha * sin(shotAngleRadians) - -displacement.y)));
- 	return initialVelocity;
 	
+ 	return initialVelocity;
 }
 
 // Calculate the shot angle to hit the target, given the following information.
@@ -75,6 +72,30 @@ float Controller::calculateShotSpeed(const Vector2& tankPos, const Vector2& enem
 float Controller::calculateShotAngle(const Vector2& tankPos, const Vector2& enemyPos, float shotSpeed, float gravity, float wind)
 {
 	// TODO: calculate the required shot angle (in radians) and return it
-	return M_PI * 0.25f;
+
+	/**
+	 * Task 4 - no wind 
+	 * using the equation from task 2 Sy = Sx * sin(x) / cos(x) - gravity * Sx^2 / 2 * u^2 * cos^2(x)
+	 * with trig idenities we can get Sy = Sx * tan(x) - gravity * Sx^2 * sec^2(x) / 2 * u^2
+	 * with another identity we can then get Sy = x * tan(x) - gravity * Sx^2 * (1 + tan^2(x)) / 2 * u^2
+	 * we can then rearrange this to equal 0
+	 * we then get (gravity * Sx^2 / 2 * u^2) * tan^2(x) - Sx * tan(x) + (gravity * Sx^2 / 2 * u^2) + y = 0
+	 * we can then use this equation with a quadratic formula
+	 * where:
+	 * a = gravity * Sx^2 / 2 * u^2
+	 * b = Sx
+	 * c = gravity * Sx^2 / 2 * u^2 + Sy
+	 */
+	const Vector2 displacement = enemyPos - tankPos;
+
+	float a = (gravity * pow(displacement.x, 2)) / (2 * pow(shotSpeed, 2));
+	float b = displacement.x;
+	float c = (gravity * pow(displacement.x, 2)) / (2 * pow(shotSpeed, 2)) + -displacement.y;
+
+	// Both of these appear to hit the target threw testing with angleCheck1 Having an actual arc to the shot
+	const float angleCheck1 = atan((b + sqrt(pow(b, 2) - 4 * a * c)) / (2 * a));
+	const float angleCheck2 = atan((b - sqrt(pow(b, 2) - 4 * a * c)) / (2 * a));
+	
+	return angleCheck1;
 }
 
